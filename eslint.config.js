2742f8a55086cb6ebea2c6110e602b38d2e85981
Angelo Silva <angelosilva.goncalves@outlook.com.br>
2025-12-04 19:57:12 -0300

    build(config): initial setup of quality tools and enviroment
    Implements ESLint, Prettier, Husky, Commitlint and Lint-Staged.

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import json from '@eslint/json';
+import css from '@eslint/css';
+import { defineConfig } from 'eslint/config';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+  { files: ['**/*.css'], plugins: { css }, language: 'css/css', extends: ['css/recommended'] },
+  eslintPluginPrettier,
+]);
