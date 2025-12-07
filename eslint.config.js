b2c67e5bdc8e9b843f12af42ed6a3ff2c3952ff3
Angelo Silva <angelosilva.goncalves@outlook.com.br>
2025-12-07 13:44:34 -0300

    fix(eslint): fixes configuration errors in eslint
    Fixes some errors in the ESLint configuration, specifically in the Jest section.
    
    The errors fixed relate to the import of Jest globals.

--- eslint.config.js
+++ eslint.config.js
@@ -7,18 +7,24 @@ import pluginPrettier from 'eslint-plugin-prettier/recommended';
 import pluginJest from 'eslint-plugin-jest';
 
 export default defineConfig([
+  js.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
   },
   {
-    files: ['**/*.spec.js', '**/*.test.js'],
+    files: ['**/*.spec.js', '**/*.test.js', '**/__tests__/**/*.js'],
     plugins: { jest: pluginJest },
-    languageOptions: { globals: pluginJest.environments.globals.globals },
+    languageOptions: {
+      globals: { ...globals.jest },
+    },
+    rules: {
+      ...pluginJest.configs['flat/recommended'].rules,
+    },
   },
-  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
-  { files: ['**/*.css'], plugins: { css }, language: 'css/css', extends: ['css/recommended'] },
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json' },
+  { files: ['**/*.css'], plugins: { css }, language: 'css/css' },
   pluginPrettier,
 ]);
